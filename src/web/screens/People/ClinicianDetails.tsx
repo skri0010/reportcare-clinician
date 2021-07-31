f74aacfcebcff68bdd9382731d5de554969a8328
Ibrahim Mohamed Ragheb Ragheb Shaarawy <isha0004@student.monash.edu>
2021-07-31 11:55:21 +0200

    [IC-152] using contact row for clinician tab

---
+++ src/web/screens/People/ClinicianDetails.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react";
+import { ScaledSheet } from "react-native-size-matters";
+import { ScreenWrapper } from "mobile/screens/ScreenWrapper";
+import { View } from "react-native";
+import { ContactTitle } from "./ContactTitle";
+
+export const ClinicianDetails: FC = () => {
+    return (
+        <ScreenWrapper>
+            <View style={{ display: "flex", flexDirection: "row", flexWrap: "wrap" }}>
+
+            </View>
+        </ScreenWrapper>
+
+    );
+}
--- src/web/screens/People/CliniciansTab.tsx
+++ src/web/screens/People/CliniciansTab.tsx
@@ -13,18 +13,9 @@ import { NoSelection } from "./NoSelection";
 import { ItemSeparator } from "components/RowComponents/ItemSeparator";
 import { ClinicianInfo } from "aws/models";
 import { createStackNavigator } from "@react-navigation/stack";
+import { ContactTitle } from "./ContactTitle";
 
 
-const Stack = createStackNavigator();
-
-export const ClincianContext = createContext({
-  id: "",
-  name: "",
-  role: "",
-  hospitalName: "",
-  clinicianID: "",
-  owner: ""
-})
 export const CliniciansTab: FC<WithSideTabsProps[ScreenName.CLINICIAN]> = () => {
   const { colors } = select((state: RootState) => ({
     colors: state.settings.colors
@@ -32,49 +23,13 @@ export const CliniciansTab: FC<WithSideTabsProps[ScreenName.CLINICIAN]> = () => 
 
   const [filteredClinicians, setFilteredClinicians] = useState<ClinicianInfo[]>(mockClinician);
 
-  const Stack = createStackNavigator();
-  const [isEmptyClinician, setEmptyClinician] = useState(true);
-  const [clinicianSelected, setClinicianSelected] = useState<ClinicianInfo>({
-    id: "",
-    name: "",
-    role: "",
-    hospitalName: "",
-    clinicianID: "",
-    owner: ""
-  });
 
   const [selectedClinician] = useState(mockClinician[0]);
 
-  const initialClinician = {
-    id: clinicianSelected.id,
-    name: clinicianSelected.name,
-    role: clinicianSelected.role,
-    hospitalName: clinicianSelected.hospitalName,
-    clinicianID: clinicianSelected.clinicianID,
-    owner: clinicianSelected.owner
-  };
 
-  function onRowClick(item: ClinicianInfo) {
-    const currentSelected = clinicianSelected;
-    const emptyClinician: ClinicianInfo = {
-      id: "",
-      name: "",
-      role: "",
-      hospitalName: "",
-      clinicianID: "",
-      owner: ""
-    };
-    if (currentSelected !== item && item !== emptyClinician) {
-      setEmptyClinician(false);
-      setClinicianSelected(item);
-    } else if (item === emptyClinician) {
-      setEmptyClinician(true);
-    }
-  }
   // JH-TODO: Replace placeholder with i18n
   return (
     <ScreenWrapper>
-<<<<<<< HEAD
       <View style={{ flexDirection: "row", height: "100%" }}>
         <View style={{ flex: 1, height: Dimensions.get("window").height }}>
           <RowSelectionWrapper
@@ -89,53 +44,18 @@ export const CliniciansTab: FC<WithSideTabsProps[ScreenName.CLINICIAN]> = () => 
               data = {filteredClinicians}
               renderItem={({ item }) => (
                 <ClinicianContactRow 
-                generalDetails={item}/>
+                generalDetails={item} />
               )
             }
               />
             </RowSelectionWrapper>
-        </View>
-        {!isEmptyClinician ? (
-          
-        )}
+            </View>
         <View style = {{ flex: 2, backgroundColor: colors.primaryWebBackgroundColor }}>
-          <NoSelection subtitle="Select Clinician for more info" screenName={ScreenName.CLINICIAN}/>
+          <ContactTitle name={selectedClinician.name} isPatient={false}/>
         </View>
-=======
-      <View>
-        <SearchBarComponent
-          onUserInput={() => {
-            null;
-          }}
-          onSearchClick={() => {
-            null;
-          }}
-          placeholder="Search clinicians"
-        />
-        <Text> Clinicians </Text>
-        <ClinicianContactRow generalDetails={mockClinician[0]} />
-        <ClinicianShareRow generalDetails={mockClinician[0]} checked />
->>>>>>> origin/jy-patients-tab
-      </View>
+        
+        </View>
     </ScreenWrapper>
   );
 };
 
-const styles = ScaledSheet.create({
-  title: {
-    fontWeight: "bold",
-    padding: "10@ms"
-  },
-  leftColumn: {
-    backgroundColor: "white",
-    height: "100%", 
-    width:"30%"
-  },
-  clinicianTitle: {
-    backgroundColor: lightColorScheme.primaryBarColor,
-    padding: "0@ms"
-  },
-  clinicianTab: {
-    flexDirection: "row"
-  }
-});
