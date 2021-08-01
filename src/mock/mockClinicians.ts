b5e3c816da015ce2d9fddc31845e428b14750ac6
Ibrahim Mohamed Ragheb Ragheb Shaarawy <isha0004@student.monash.edu>
2021-08-01 12:58:48 +0200

    [IC-152] added more mock clincians

--- src/mock/mockClinicians.ts
+++ src/mock/mockClinicians.ts
@@ -8,4 +8,21 @@ export const mockClinician: ClinicianInfo[] = [
     hospitalName: "Sabah Hospital",
     clinicianID: "20",
     owner: "?"
-}];
+  },
+  {
+    id: "jennny@gmail.com",
+    name: "Jenny Lin",
+    role: "Nurse",
+    hospitalName: "Queen Elizabeth II",
+    clinicianID: "11",
+    owner: "?"
+  },
+  {
+    id: "alisia@gmail.com",
+    name: "Dr Alisia",
+    role: "Pharmacist",
+    hospitalName: "Queen Elizabeth II",
+    clinicianID: "12",
+    owner: "?"
+  }
+];
