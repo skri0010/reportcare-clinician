de19a24825ba731d77698fa94c2ba554fef45251
Ibrahim Mohamed Ragheb Ragheb Shaarawy <isha0004@student.monash.edu>
2021-08-11 08:28:31 +0200

    [IC-200] all frontend functionality done, UI imporvements needed

--- src/web/screens/Alerts/AlertDetailsScreen.tsx
+++ src/web/screens/Alerts/AlertDetailsScreen.tsx
@@ -9,9 +9,15 @@ import { mockSymptomRecords } from "mock/mockSymptoms";
 import { mockVitals } from "mock/mockVitals";
 import { mockAlerts } from "mock/mockAlerts";
 import { AlertDetails } from "./AlertDetails";
+import { CardWrapper } from "../Home/CardWrapper";
+import i18n from "util/language/i18n";
 
 
 export const AlertDetailsScreen: FC = () => {
+    const { colors, fonts } = select((state: RootState) => ({
+        colors: state.settings.colors,
+        fonts: state.settings.fonts
+      }));
     const context = useContext(AlertContext);
     const findSymptoms = (vitalsReportID: string, symptomReportID: string) => {
         // TODO: to be replaced with an API or agent call
@@ -42,7 +48,21 @@ export const AlertDetailsScreen: FC = () => {
                 <H3 text="Alert Summary" style={styles.headers} />
                 <H4 text={context.summary} />
                 <H3 text="Alert Details" style={styles.informationTitle} />
+                <CardWrapper>
                 {findSymptoms(context.vitalsReportID, context.symptomReportID)}
+                </CardWrapper>
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity
+                    style={[
+                        styles.button,
+                        { backgroundColor: colors.primaryTodoCompleteButtonColor } ]}
+                        onPress={() => {
+                            // Call on add todo screen
+                          }}
+                     >
+                         <H4 text="Create Todo" style={{ color: colors.primaryContrastTextColor }} />
+                    </TouchableOpacity>
+                </View>
             </View>
         </ScreenWrapper>
     );
@@ -63,6 +83,18 @@ const styles = ScaledSheet.create({
         paddingBottom: "17@ms"
     },
     informationTitle: {
-        fontWeight: "bold"
+        fontWeight: "bold",
+        paddingTop: "17@ms"
+    },
+    buttonContainer: {
+        marginTop: "10@ms",
+        alignItems: "center"
+    },
+    button: {
+        textAlign: "center",
+        justifyContent: "space-evenly",
+        borderRadius: "5@ms",
+        width: "80@ms",
+        height: "30@ms"
     }
 });
