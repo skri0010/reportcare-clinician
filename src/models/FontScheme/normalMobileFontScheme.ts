ed2668d0501cb53e3195c1c8dda0a1dfda58b910
Jeng Yue <jong0021@student.monash.edu>
2021-10-18 00:58:58 +0800

    [IC-289] Revert font change

--- src/models/FontScheme/normalMobileFontScheme.ts
+++ src/models/FontScheme/normalMobileFontScheme.ts
@@ -6,7 +6,7 @@ export const normalMobileFontScheme: FontScheme = {
   h1Size: ms(24),
   h2Size: ms(22),
   h3Size: ms(20),
-  h4Size: ms(400),
+  h4Size: ms(18),
   h5Size: ms(16),
   h6Size: ms(14),
   h7Size: ms(12),
