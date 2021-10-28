ba85a43367253e496a285701a8750ca4843cff0c
Lim Jian Hui <limjianhui99@gmail.com>
2021-10-28 12:49:56 +0800

    [IC-299] Amplify pull

--- amplify/backend/function/patientassignmenthandler/src/utility.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.createNewEventResponse = void 0;
-var createNewEventResponse = function (success) {
-    if (success === void 0) { success = false; }
-    return { success: success };
-};
-exports.createNewEventResponse = createNewEventResponse;
