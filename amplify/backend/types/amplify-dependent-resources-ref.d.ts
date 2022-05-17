7355c4c9bf1beffe0bdb71adab811fb8ce7c16ab
daikidev111 <dkub0001@student.monash.edu>
2022-05-17 16:49:45 +0900

    updated the dependent resources

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,41 +1,41 @@
 export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "reportcarecee80c6fcee80c6f": {
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string",
-            "CreatedSNSRole": "string"
-        }
-    },
-    "api": {
-        "reportcare": {
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    },
-    "storage": {
-        "s3clinicianrecords": {
-            "BucketName": "string",
-            "Region": "string"
-        }
-    },
-    "function": {
-        "reportcarelambdalayer": {
-            "Arn": "string"
-        },
-        "patientassignmenthandler": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        },
-        "s3clinicianrecordsbridge": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        }
-    }
-}
+  auth: {
+    reportcarecee80c6fcee80c6f: {
+      UserPoolId: "string";
+      UserPoolArn: "string";
+      UserPoolName: "string";
+      AppClientIDWeb: "string";
+      AppClientID: "string";
+      CreatedSNSRole: "string";
+    };
+  };
+  api: {
+    reportcare: {
+      GraphQLAPIIdOutput: "string";
+      GraphQLAPIEndpointOutput: "string";
+    };
+  };
+  storage: {
+    s3clinicianrecords: {
+      BucketName: "string";
+      Region: "string";
+    };
+  };
+  function: {
+    reportcarelambdalayer: {
+      Arn: "string";
+    };
+    patientassignmenthandler: {
+      Name: "string";
+      Arn: "string";
+      Region: "string";
+      LambdaExecutionRole: "string";
+    };
+    s3clinicianrecordsbridge: {
+      Name: "string";
+      Arn: "string";
+      Region: "string";
+      LambdaExecutionRole: "string";
+    };
+  };
+};
